37d7aa08fb00f30fe286d184884e7f9b41f2b1dd
dorisnorris <norris.doris@outlook.com>
2021-07-30 13:26:39 -0700

    planet multi-select phase 3

--- app/plots.py
+++ app/plots.py
@@ -10,10 +10,23 @@ def test_plot(selected_planet):
 	x = np.arange(10)
 	fig = go.Figure(data=go.Scatter(x=x, y=x**2))
 
+	if len(selected_planet) == 0:
+		title= "None Selected"
+		print(title)
+	elif len(selected_planet) == 1:
+		title= selected_planet[0]
+		print(title)
+	elif len(selected_planet) == 2:
+		title= ' and '.join(selected_planet)
+		print(title)
+	elif len(selected_planet) > 2:
+		title=  selected_planet[0] + ', ' + selected_planet[1] + ', and ' + selected_planet[2]
+		print(title)
+
 	fig.update_layout(
                  xaxis_title="X axis",
                  yaxis_title="Y axis",
-                 title= selected_planet)
+                 title= title)
 
 	graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
 
