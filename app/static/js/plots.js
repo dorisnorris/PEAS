987179c03c4a7676dae786034a38cd78d36671ac
dorisnorris <norris.doris@outlook.com>
2021-07-29 19:06:05 -0700

    multi-select phase 2

--- app/static/js/plots.js
+++ app/static/js/plots.js
@@ -2,16 +2,17 @@ $(function () {
     $('.selectpicker').selectpicker();
 });
 
-$('#SelectPlanet').on('click',function(){
+$('#SelectPlanet').on('change',function(){
+    console.log("HELLO");
     // get multi select values 
     var selected = $('#SelectPlanet').find("option:selected"); //get current selected value
-    console.log(selected);
     var arrSelectedPlanet = []; //Array to store your multiple value in stack
     selected.each(function(){
     arrSelectedPlanet.push($(this).val()); //Stack the value
     });
 
 // $('#SelectPlanet').on('change',function(){
+    console.log(arrSelectedPlanet);
 
     $.ajax({
         url: "/planet", 
